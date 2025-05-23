8600065482455ad9a8f81e1e516ce1260e2dedbd
yevhenii vorobiov <yevhenii.vorobiov@bilton.tech>
2025-05-23 11:38:09 +0300

    feat: grid change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import MainMenu from "./components/MainMenu";
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-blue-950 text-white p-4">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-blue-950 text-white">
       <MainMenu />
     </main>
   );
--- src/components/RoundPage.tsx
+++ src/components/RoundPage.tsx
@@ -4,6 +4,7 @@ import Timer from "@/components/Timer";
 import gif from "@/assets/gifs/video.gif";
 import { X } from "lucide-react";
 import { LocalStoreService } from "@/lib/LocalstoreService";
+import { cn } from "@/lib/utils";
 
 interface Participant {
   id: number;
@@ -21,11 +22,48 @@ interface RoundPageProps {
   round: RoundConfig;
   onReturnToMenu: () => void;
 }
+const smallDevice = window.innerWidth < 400;
+
+const getChunkedParticipants = (participants: Participant[]) => {
+  const chunkedParticipants = [];
+  let flag = true;
+
+  try {
+    for (let i = 0; i < participants.length; i++) {
+      if (smallDevice) {
+        if (flag) {
+          chunkedParticipants.push([participants[i], participants[i + 1]]);
+          i++;
+        } else {
+          chunkedParticipants.push([participants[i]]);
+        }
+      } else {
+        if (flag) {
+          chunkedParticipants.push([
+            participants[i],
+            participants[i + 1],
+            participants[i + 2],
+          ]);
+          i += 2;
+        } else {
+          chunkedParticipants.push([participants[i], participants[i + 1]]);
+          i++;
+        }
+      }
+      flag = !flag;
+    }
+  } catch (e) {
+    console.log("first", e);
+  }
+
+  return chunkedParticipants;
+};
 
 export default function RoundPage({ round, onReturnToMenu }: RoundPageProps) {
   const [selectedParticipants, setSelectedParticipants] = useState<number[]>(
     []
   );
+  const chunkedParticipants = getChunkedParticipants(round.participants);
 
   const [showResult, setShowResult] = useState(false);
   const [timeExpired, setTimeExpired] = useState(false);
@@ -48,25 +86,57 @@ export default function RoundPage({ round, onReturnToMenu }: RoundPageProps) {
     setShowResult(true);
   };
 
+  const getImage = (p: Participant) => {
+    return p ? (
+      <div>
+        {selectedParticipants.includes(p.id) && (
+          <button
+            onClick={() => toggleParticipant(p.id)}
+            disabled={timeExpired}
+            className="bg-red-500 absolute rounded-full p-1 transform z-10 border-0 rotate-45 ml-[1dvw] mt-[1dvw]"
+          >
+            <X size={16} />
+          </button>
+        )}
+        <img
+          onClick={() => toggleParticipant(p.id)}
+          key={p.id}
+          src={p.image}
+          alt={p.name}
+          className={`transition-all duration-200 border-1 border-blue-400 ${
+            selectedParticipants.includes(p.id)
+              ? "grayscale"
+              : "shadow-lg shadow-green-400/30"
+          }`}
+        />
+      </div>
+    ) : (
+      <div className=""></div>
+    );
+  };
   const handleTimeExpired = () => {
     setTimeExpired(true);
-    // Auto-confirm when time expires
-    if (selectedParticipants.length === round.requiredSelections) {
-      setShowResult(true);
-    }
+    setShowResult(true);
+    LocalStoreService.setImages(`round-${round.id}`, ["time", "expired"]);
   };
 
   if (showResult) {
     return (
       <div className="flex flex-col items-center justify-center w-full max-w-4xl">
         <div className="mb-8 w-full max-w-md">
-          <img
-            src={gif}
-            alt="Result GIF"
-            width={400}
-            height={300}
-            className="mx-auto rounded-lg"
-          />
+          {timeExpired ? (
+            <span className="text-2xl font-bold text-center">
+              Час закінчився!
+            </span>
+          ) : (
+            <img
+              src={gif}
+              alt="Result GIF"
+              width={400}
+              height={300}
+              className="mx-auto rounded-lg"
+            />
+          )}
         </div>
         <Button
           onClick={onReturnToMenu}
@@ -79,57 +149,51 @@ export default function RoundPage({ round, onReturnToMenu }: RoundPageProps) {
   }
 
   return (
-    <div className="flex flex-col gap-10 items-center justify-center w-full max-w-4xl">
-      <h1 className="text-3xl font-bold">Обрати учасників</h1>
+    <div className="flex flex-col gap-10 items-center justify-center w-full">
+      <h1 className="text-3xl font-bold">Оберіть учасників</h1>
 
       <div className="mb-6">
         <Timer duration={60} onExpire={handleTimeExpired} />
       </div>
-
-      <div className="flex gap-12 flex-wrap justify-center items-center">
-        {round.participants.map((participant) => (
-          <div
-            key={participant.id}
-            className="relative flex justify-center gap-4"
-          >
-            <button
-              onClick={() => toggleParticipant(participant.id)}
-              disabled={timeExpired}
-              className={`
-                transform -rotate-45 aspect-square relative overflow-hidden h-20 w-20 border-2 border-blue-400
-                ${
-                  selectedParticipants.includes(participant.id)
-                    ? "grayscale"
-                    : "shadow-lg shadow-green-400/30"
-                }
-                ${
-                  timeExpired
-                    ? "opacity-50 cursor-not-allowed"
-                    : "hover:scale-105"
-                }
-                transition-all duration-200
-              `}
+      <div className="flex flex-col justify-around">
+        {chunkedParticipants.map((chunk, index) =>
+          smallDevice ? (
+            <div
+              className={cn(
+                "flex w-full items-center justify-center",
+                index !== 0 ? "-mt-[18dvw]" : ""
+              )}
             >
-              <div className="absolute inset-0 transform flex items-center justify-center overflow-hidden">
-                <img
-                  src={participant.image || "/placeholder.svg"}
-                  alt={participant.name}
-                  className="min-w-full min-h-full object-cover"
-                />
+              <div className="">
+                <div
+                  className={cn(
+                    "flex flex-wrap gap-[4dvw] *:h-[42dvw] *:w-[42dvw] *:scale-[70%] *:rotate-45"
+                  )}
+                >
+                  {chunk.map((p) => getImage(p))}
+                </div>
               </div>
-            </button>
-
-            {selectedParticipants.includes(participant.id) && (
-              <button
-                onClick={() => toggleParticipant(participant.id)}
-                disabled={timeExpired}
-                className="absolute -top-3 -right-3 bg-red-500 rounded-full p-1 transform -rotate-90 z-10"
-              >
-                <X size={16} />
-              </button>
-            )}
-          </div>
-        ))}
+            </div>
+          ) : (
+            <div
+              className={cn(
+                "flex w-full items-center justify-center",
+                index !== 0 ? "-mt-[12dvw]" : ""
+              )}
+            >
+              <div className="">
+                <div
+                  className={cn(
+                    "flex flex-wrap gap-[4dvw] *:h-[30dvw] *:w-[30dvw] *:scale-[70%] *:rotate-45",
+                    index % 2 !== 0 ? "-mt-[1dvw]" : ""
+                  )}
+                >
+                  {chunk.map((p) => getImage(p))}
+                </div>
+              </div>
+            </div>
+          )
+        )}
       </div>
 
       <div className="text-xl">
